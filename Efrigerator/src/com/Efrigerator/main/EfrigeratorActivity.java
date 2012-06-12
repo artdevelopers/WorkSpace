2c5060d4a9c54ab783d8a634045cb9221e2a8af8
artdevelopers <artdevelopers111@gmail.com>
2012-06-11 23:44:52 -0400

    06112012

---
+++ Efrigerator/src/com/Efrigerator/main/EfrigeratorActivity.java
@@ -0,0 +1,13 @@
+package com.Efrigerator.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EfrigeratorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
