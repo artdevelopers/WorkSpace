6d5777c6e1a95ab74a08a3c4cbbed96211f029b4
artdevelopers <artdevelopers111@gmail.com>
2012-06-12 08:03:00 -0400

    20120612 AAT adding sound effect to Fish

--- Efrigerator/src/com/Efrigerator/main/EfrigeratorActivity.java
+++ Efrigerator/src/com/Efrigerator/main/EfrigeratorActivity.java
@@ -1,7 +1,10 @@
 package com.Efrigerator.main;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EfrigeratorActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,19 @@ public class EfrigeratorActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final MediaPlayer mpButtonClick = MediaPlayer.create(this, R.raw.button7);
+        
+        Button Fish = (Button) findViewById(R.id.fish);
+        
+        Fish.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			 mpButtonClick.start();	
+			}
+		});
+        
+        
     }
 }
